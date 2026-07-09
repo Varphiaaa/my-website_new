e3b6ae997498b036d8ed15908aa51eca60f0ed83
Varphiaaa <150934795+Varphiaaa@users.noreply.github.com>
2026-07-09 14:26:01 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
         comment: { ja: '日本の数学研究の拠点', en: 'Hub of mathematical research in Japan' } 
       },
       { 
-        src: 'images/DSC04243.JPG', 
+        src: 'images/007B31EA-20E9-4399-88E3-A55CE3C59F99_1_105_c.jpeg', 
         comment: { ja: '日常の研究のひとこま', en: 'A glimpse of daily research' } 
       }
     ];
